5d7b6d637d819867760cc3bcb21f6c6144f09cc5
Klaus Gubernu <claudiugubernu@gmail.com>
2022-02-02 22:43:49 +0000

    added admin logo, admin index.php, admin-style folder, admin footer, admin header

---
+++ admin/index.php
@@ -0,0 +1,61 @@
+<?php
+
+session_start(); 
+
+include_once('../includes/connection.php');
+
+if (isset($_SESSION["logged_in"])) {
+    include('templates/admin-dashboard.php');
+} else {
+    if (isset($_POST['username'], $_POST['password'])) {
+        $username = $_POST['username'];
+        $password = md5($_POST['password']);
+
+        if (empty($username) or empty($password)) {
+            $error = 'All fields are required';
+        } else {
+            $query = $pdo->prepare('SELECT * FROM users WHERE user_name = ? AND user_password = ?');
+            $query->bindValue(1, $username);
+            $query->bindValue(2, $password);
+            $query->execute();
+            $num = $query->rowCount();
+            if ($num == 1) {
+                $_SESSION['logged_in'] = true;
+                $_SESSION['username'] = $username;
+                header('Location: index.php');
+                exit();
+            } else {
+                $error = 'Incorrect details entered';
+            }
+        }
+    }
+    ?>
+
+<?php include('header.php'); ?>
+
+<nav>
+    <div class="nav-wrapper">
+        <a href="index.php">
+            <img src="../assets/img/Logo2 Admin.png" alt="Site logo"/>
+        </a>
+        <a href="admin/">Sign In</a>
+    </div>
+</nav>
+
+<?php if (isset($error)) { ?>
+    <div class="login-error">
+        <p><?php echo $error ?></p>
+    </div>
+<?php } ?>
+
+    <form action="index.php" method="post"> 
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="password" placeholder="Password">
+        <input type="submit" value="Login">
+    </form>
+
+<?php include('footer.php'); ?> 
+
+<?php }
+
+
