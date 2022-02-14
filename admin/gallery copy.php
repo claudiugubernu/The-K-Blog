309fbefe23365f8cbef4d718aa2f64fb7ca02a28
Klaus Gubernu <claudiugubernu@gmail.com>
2022-02-14 13:58:35 +0000

    added user and cms settings pages and few tweaks on front end

--- admin/gallery copy.php
+++ admin/gallery copy.php
@@ -3,29 +3,31 @@
 session_start();
 
 include_once('../includes/connection.php');
+include_once('../includes/posts.php');
 
 if (isset($_SESSION["logged_in"])) { 
     $username = $_SESSION['username'];
-    $dir = './uploads/*.jpg';
-    $images = glob( $dir );
+
+     // Get Posts so we're able to get necessary details for admin dashboard
+     $db_posts = new Posts;
+     $posts = $db_posts->fetch_all();
 
 } else {
     header('Location: index.php');
 }
 
+$i=0;
 ?>
 
 <?php include('header.php'); ?>
 
 <div class="posts-wrapper admin-wrapper admin-gallery-wrapper flex align-items-center bg-tertiary">
     <?php include('templates/admin-nav.php'); ?>
-    <div class="admin-dashboard-wrapper p-30 flex flex-column">
-        <?php include('templates/admin-logout.php'); ?> 
-        <div class="admin-gallery grid">
-        <?php foreach ($images as $image) { ?> 
-            <?php if ($image) { ?>
+        <div class="admin-gallery p-30 mv-50 grid">
+        <?php foreach ($posts as $post) { ?> 
+            <?php if ($post['post_thumbnail']) { ?>
                 <div class="admin-gallery-img">
-                    <img src="<?php echo $image ?>" class="single-post-img" />
+                    <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($post['post_thumbnail']); ?>" class="single-post-img" />
                 </div>    
             <?php } ?>
         <?php } ?>
---
+++ admin/general-settings.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+
+include_once('../includes/connection.php');
+
+if (isset($_SESSION["logged_in"])) { 
+    $username = $_SESSION['username'];
+} else {
+    header('Location: index.php');
+}
+
+?>
+
+<?php include('header.php'); ?>
+
+<div class="users-wrapper admin-wrapper flex align-items-center bg-tertiary">
+    <?php include('templates/admin-nav.php'); ?>
+    <div class="admin-dashboard-wrapper p-30 flex flex-column">
+        <?php include('templates/admin-logout.php'); ?> 
+        <form method="post" class="cms-update-form flex flex-column justify-center">
+            Banner img settings <br>
+            Site title settings<br>
+            Theme change selector ?<br>
+            <input type="submit" name="submit" value="UPDATE" class="btn green-btn mv-20"/>
+        </form>
+    </div>
+</div>
+
+<?php include('footer.php'); ?>
