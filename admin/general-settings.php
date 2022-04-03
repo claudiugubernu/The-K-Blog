0694503bb0df1bf45e7b613b164c3b4dd9c206c4
Klaus Gubernu <claudiugubernu@gmail.com>
2022-04-03 16:48:10 +0100

    general settings and improvments

--- admin/general-settings.php
+++ admin/general-settings.php
@@ -6,6 +6,34 @@ include_once('../includes/connection.php');
 
 if (isset($_SESSION["logged_in"])) { 
     $username = $_SESSION['username'];
+
+    // Prepare query to get settings from DB
+    $query = $pdo->prepare('SELECT * FROM cms_settings');
+    $query->execute();
+
+    // Get all CMS settings and assign them
+    $cms_settings = $query->fetchAll();
+
+    // Update CMS settings in DB
+    if (isset($_POST['site_info'])) {
+        if (!empty($_POST['site_title'])) {
+            $site_title = $_POST['site_title'];
+
+            // Prepare query to get settings from DB
+            $query = $pdo->prepare('UPDATE cms_settings SET site_title=?');
+            $query->bindValue(1, $site_title);
+
+            $query_execute = $query->execute();
+
+            if ($query_execute) {
+                header('Location: general-settings.php');
+            } else {
+                $error_message =  'Unknown error. Please try again.';
+            }
+
+
+        }
+    }
 } else {
     header('Location: index.php');
 }
@@ -14,16 +42,30 @@ if (isset($_SESSION["logged_in"])) {
 
 <?php include('header.php'); ?>
 
-<div class="users-wrapper admin-wrapper flex align-items-center bg-tertiary">
+<div class="general-settings-wrapper admin-wrapper flex align-items-center bg-tertiary">
     <?php include('templates/admin-nav.php'); ?>
     <div class="admin-dashboard-wrapper p-30 flex flex-column">
         <?php include('templates/admin-logout.php'); ?> 
         <form method="post" class="cms-update-form flex flex-column justify-center">
-            Banner img settings <br>
-            Site title settings<br>
-            Theme change selector ?<br>
-            site favicon ?<br>
-            <input type="submit" name="submit" value="UPDATE" class="btn green-btn mv-20"/>
+            <div class="form-row flex flex-column">
+                <label for="favicon" class="tooltip mb-10 fs-16 c-light-grey">Site Icon <span class="info"> &#9432;</span><span class="tooltiptext">Site Icons should be square and at least 512 × 512 pixels.</span></label>
+                <input type="file" name="favicon" class="mb-20" accept="image/png, image/jpeg, image/jpg" id="favicon"/>
+            </div>
+            <div class="form-row flex flex-column">
+                <label for="site_title" class="mb-10 fs-16 c-light-grey">Site Title</label>
+                <input type="text" name="site_title" class="mb-10 p-10 bg-senary c-light-grey" value="<?php echo $cms_settings[0]['site_title']; ?>"/>
+            </div>
+            <input type="submit" name="site_info" value="UPDATE" class="btn green-btn mv-20"/>
+        </form>
+
+        <form method="post" class="cms-update-form flex flex-column justify-center">
+            <label class="mb-10 fs-16 c-light-grey">Select CMS Theme</label>
+            <select name="theme-id" id="theme-id">
+                <option selected>Default</option>
+                <option value="">Corporate</option>
+                <option value="">Special 1</option>
+                <option value="">Special 2</option>
+            </select>
         </form>
     </div>
 </div>
--- header.php
+++ header.php
@@ -1,10 +1,22 @@
+<?php
+include_once('includes/connection.php');
+
+// Prepare query to get settings from DB
+$query = $pdo->prepare('SELECT * FROM cms_settings');
+$query->execute();
+
+// Get all CMS settings and assign them
+$cms_settings = $query->fetchAll();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>The K Blog</title>
+        <title><?php echo $cms_settings[0]['site_title']; ?></title>
         <link rel="stylesheet" href="static/app.css" />
     </head>
         <body>
