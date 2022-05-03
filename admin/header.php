8ce454c62ada8d7f0dc0ffe2881b6e41d4e120f8
Klaus Gubernu <claudiugubernu@gmail.com>
2022-05-03 21:31:18 +0100

    added node_modules to gitignore

--- admin/header.php
+++ admin/header.php
@@ -16,7 +16,7 @@ $cms_settings = $query->fetchAll();
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?php echo $cms_settings[0]['site_title']; ?>- Admin</title>
+        <title><?php echo $cms_settings[0]['site_title']; ?> - Admin</title>
         <link rel="shortcut icon" type="image/x-icon" href="<?php echo $cms_settings[0]['site_icon'] ?>"/>
         <link rel="stylesheet" href="../static/app.css" />
     </head>
