ee1aa8a4034f63695627219fcd173f2eeb084ba2
Klaus Gubernu <claudiugubernu@gmail.com>
2022-05-06 22:40:28 +0100

    restyled the navbar

--- admin/templates/admin-nav.php
+++ admin/templates/admin-nav.php
@@ -2,26 +2,26 @@
     <div class="admin-nav-wrapper-top flex justify-center align-items-center gap-30">
         <img src="../assets/img/Logo1 Admin.png" class="admin-logo" alt="Admin Logo" /> 
     </div>
-    <div class="admin-nav-wrapper-bottom flex-1">
-        <ul class="admin-nav-items flex flex-column justify-center fs-24">
-            <li class="admin-nav-item flex gap-10 align-items-center mv-10">
-                <img src="../../static/icons/overview.svg" class="icon-30 overview" alt="Overview Dashboard icon">
+    <div class="admin-nav-wrapper-bottom flex-1 mt-30">
+        <ul class="admin-nav-items flex flex-column fs-16">
+            <li class="admin-nav-item flex gap-10 align-items-center pv-20">
+                <img src="../../static/icons/overview.svg" class="icon-20 overview" alt="Overview Dashboard icon">
                 <a href="index.php" class="c-light-grey">Overview</a>
             </li>
-            <li class="admin-nav-item flex gap-10 align-items-center mv-10">
-                <img src="../../static/icons/add-post.svg" class="icon-30 add-post" alt="Add post icon">
+            <li class="admin-nav-item flex gap-10 align-items-center pv-20">
+                <img src="../../static/icons/add-post.svg" class="icon-20 add-post" alt="Add post icon">
                 <a href="posts.php" class="c-light-grey">Posts</a>
             </li>
-            <li class="admin-nav-item flex gap-10 align-items-center mv-10">
-                <img src="../../static/icons/gallery.svg" class="icon-30 add-post" alt="Add post icon">
+            <li class="admin-nav-item flex gap-10 align-items-center pv-20">
+                <img src="../../static/icons/gallery.svg" class="icon-20 add-post" alt="Add post icon">
                 <a href="gallery.php" class="c-light-grey">Gallery</a>
             </li>
-            <li class="admin-nav-item flex gap-10 align-items-center mv-10">
-                <img src="../../static/icons/user-settings.svg" class="icon-30 user-settings" alt="User Settings icon">
+            <li class="admin-nav-item flex gap-10 align-items-center pv-20">
+                <img src="../../static/icons/user-settings.svg" class="icon-20 user-settings" alt="User Settings icon">
                 <a href="user-settings.php" class="c-light-grey">User Setting</a>
             </li>
-            <li class="admin-nav-item flex gap-10 align-items-center mv-10">
-                <img src="../../static/icons/setting-tool.svg" class="icon-30 setting-tool" alt="Settings Tool icon">
+            <li class="admin-nav-item flex gap-10 align-items-center pv-20">
+                <img src="../../static/icons/setting-tool.svg" class="icon-20 setting-tool" alt="Settings Tool icon">
                 <a href="general-settings.php" class="c-light-grey">Settings</a>
             </li>
         </ul>
