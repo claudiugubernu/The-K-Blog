a1f83fcf63f3ec1b555b6d14de1c57a47c545100
Klaus Gubernu <claudiugubernu@gmail.com>
2022-05-06 22:02:12 +0100

    Clicking on name takes you to user settings

--- admin/templates/admin-logout.php
+++ admin/templates/admin-logout.php
@@ -1,4 +1,4 @@
 <div class="admin-dashboard-top flex justify-end align-items-center w-100 bg-white mb-50 pr-40">
-    <p class="admin-sign-out admin-c-secondary">Hello, <?php echo $username ?></p>
+    <a href="user-settings.php" class="admin-sign-out admin-c-secondary">Hello, <?php echo $username ?></a>
     <a href="logout.php" class="admin-sign-out-link">(logout)</a>
 </div>
