014c21da9f921d00319ea132ea5579f835cc631c
Klaus Gubernu <claudiugubernu@gmail.com>
2022-07-23 07:21:49 +0100

    rearranged date and title

--- single.php
+++ single.php
@@ -17,13 +17,13 @@ if (isset($_GET['id'])) {
 
 <div id="content" class="single-post-wrapper mb-50">
     <div class="post-banner relative">
+        <div class="post-info">
+            <time  datetime="<?php echo date('j F Y', $posts_data['post_timestamp']); ?>" class="post-date fs-14 tt-italic mv-20"><?php echo date('j F Y', $posts_data['post_timestamp']); ?></time>
+            <h1 class="post-title fs-50"><?php echo $posts_data['post_title']?></h1>
+        </div>
         <?php if($posts_data['post_thumbnail_path']) { ?>
             <img src="./admin/<?php echo $posts_data['post_thumbnail_path']; ?>" class="single-post-img" />
         <?php } ?>
-        <div class="post-info">
-            <h1 class="post-title fs-50"><?php echo $posts_data['post_title']?></h1>
-            <p class="post-date fs-14 tt-italic mv-20">Posted on: <?php echo date('F j, Y ', $posts_data['post_timestamp']); ?></p>
-        </div>
     </div>
     <div class="post-content site-width mv-50">
         <p class="w-50 m-w-100"><?php echo $posts_data['post_content']?></p>
